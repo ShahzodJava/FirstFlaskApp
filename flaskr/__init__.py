03ddc3b64f553f34f08a986d4ff034b385656164
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2022-01-07 12:43:05 +0500

    added app.route  for plant with exact id

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from models import setup_db, Plant
 from flask_cors import CORS
 
@@ -16,13 +16,31 @@ def create_app(test_config=None):
 
   @app.route('/plants')
   def get_plants():
+
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * 10
+    end = start + 10
+
     plants = Plant.query.all()
 
     formatted_plants = [plant.format() for plant in plants]
 
     return jsonify({
       'success': True,
-      'plants': formatted_plants
+      'plants': formatted_plants[start:end],
+      'total_plants': len(formatted_plants)
     })
+
+  @app.route('/plants/<int:plant_id>', methods=['GET'])
+  def get_plant(plant_id):
+    plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+
+    if plant is None:
+      abort(404)
+    else:
+      return jsonify({
+        'success':True,
+        'plant': plant.format()
+      })
 
   return app
