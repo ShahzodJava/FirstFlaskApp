12a46ebba4207d8d80dc053773f35be4505fc4a6
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2022-01-07 11:04:57 +0500

    implemented models.py and api of plants

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,28 @@
 from flask import Flask, jsonify
+from models import setup_db, Plant
 from flask_cors import CORS
 
 def create_app(test_config=None):
   app=Flask(__name__)
 
-  @app.route('/')
-  def hello():
-    return jsonify({'message': 'HELLO WORLD'})
+  setup_db(app)
+  CORS(app)
 
-  @app.route('/smile')
-  def smile():
-    return ':)'
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
+
+  @app.route('/plants')
+  def get_plants():
+    plants = Plant.query.all()
+
+    formatted_plants = [plant.format() for plant in plants]
+
+    return jsonify({
+      'success': True,
+      'plants': formatted_plants
+    })
 
   return app
