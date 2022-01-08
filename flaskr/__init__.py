1295fed3d2a46e1657d1bb85bd35d8ed277978a6
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2022-01-08 16:14:26 +0500

    added app.routes to create, update, and delete plants, also added tests to all of them to check working procces

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,15 @@
+from sys import meta_path
 from flask import Flask, jsonify, request, abort
 from models import setup_db, Plant
 from flask_cors import CORS
+
+def paginate_plants(request,p):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * 5
+  end = start + 5
+  formatted_plants = [plant.format() for plant in p]
+  current_plants = formatted_plants[start:end]
+  return current_plants
 
 def create_app(test_config=None):
   app=Flask(__name__)
@@ -16,19 +25,16 @@ def create_app(test_config=None):
 
   @app.route('/plants')
   def get_plants():
+    p = Plant.query.order_by(Plant.id).all()
+    current_plants = paginate_plants(request, p)
 
-    page = request.args.get('page', 1, type=int)
-    start = (page - 1) * 10
-    end = start + 10
-
-    plants = Plant.query.all()
-
-    formatted_plants = [plant.format() for plant in plants]
+    if len(current_plants) == 0:
+      abort(404)
 
     return jsonify({
       'success': True,
-      'plants': formatted_plants[start:end],
-      'total_plants': len(formatted_plants)
+      'plants': current_plants,
+      'total_plants': len(Plant.query.all())
     })
 
   @app.route('/plants/<int:plant_id>', methods=['GET'])
@@ -41,6 +47,89 @@ def create_app(test_config=None):
       return jsonify({
         'success':True,
         'plant': plant.format()
+
       })
+
+  @app.route('/plants/<int:plant_id>', methods=['PATCH'])
+  def update_plant(plant_id):
+    body = request.get_json()
+
+    try:
+      plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+      if plant is None:
+        abort(404)
+
+      if "is_poisonous" in body:
+          plant.is_poisonous = body.get("is_poisonous")
+      plant.update()
+
+      return jsonify( { "success":True, "id":plant.id })
+    except:
+      abort(400)
+
+  @app.route('/plants/<int:plant_id>', methods=['DELETE'])
+  def delete_plant(plant_id):
+    try:
+      plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+      if plant is None:
+        abort(404)
+      plant.delete()
+      return jsonify({
+        'success':True,
+        "id":plant_id
+      })
+    except:
+      abort(422)
+
+  @app.route('/plants', methods=['POST'])
+  def create_new_plant():
+    body = request.get_json()
+    name = body.get('name')
+    scientific_name=body.get('scientific_name')
+    is_poisonous = body.get('is_poisonous')
+    primary_color = body.get('primary_color')
+    try:
+
+      plant = Plant(name=name, scientific_name=scientific_name, is_poisonous=is_poisonous,primary_color=primary_color)
+      plant.insert()
+
+      return jsonify({
+        "success":True,
+        "created":plant.id
+      })
+    except:
+      abort(422)
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return (jsonify({
+      "success":False,
+      "error":404,
+      "message":"resource not found"
+    }), 404)
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return (jsonify({
+      "success":False,
+      "error":422,
+      "message":"unprocessable"
+    }), 422)
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return (jsonify({
+      "success":False,
+      "error":400,
+      "message":"bad request"
+    }), 400)
+
+  @app.errorhandler(405)
+  def not_found(error):
+    return (
+      jsonify({"success": False, "error": 405, "message": "method not allowed"}),
+      405,
+    )
+
 
   return app
---
+++ test_app.py
@@ -0,0 +1,114 @@
+import unittest
+import json
+
+from werkzeug.test import Client
+from flaskr import create_app
+from models import setup_db, Plant
+
+class PlantTestCase(unittest.TestCase):
+
+  def setUp(self):
+    self.app = create_app()
+    self.client = self.app.test_client
+    self.database_name = "plant_test"
+    self.database_path = "postgresql://{}:{}@{}/{}".format('postgres','7907','localhost:5432',self.database_name)
+    setup_db(self.app, self.database_path)
+
+    self.new_plant = {
+      'name':'Margarite',
+      'scientific_name': 'Salvia margaritae Botsch',
+      'is_poisonous':False,
+      'primary_color':'purple'
+    }
+  def tearDown(self):
+    pass
+  def test_get_plants(self):
+    res = self.client().get('/plants')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'],True)
+    self.assertTrue(data['total_plants'])
+    self.assertTrue(len(data['plants']))
+
+  def test_404_send_requesting_beyond_valid_page(self):
+    res = self.client().get('/plants?page=10000')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 404)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'],'resource not found')
+
+  def test_get_plant(self):
+    res = self.client().get('/plants/17', json={'primary_color':'blue'})
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(len(data['plant']))
+
+  def test_404_send_requesting_beyond_valid_plant_id(self):
+    res = self.client().get('/plants/100')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 404)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'resource not found')
+
+  def test_update_plant(self):
+    res = self.client().patch('/plants/17', json={'is_poisonous':False})
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['id'])
+
+  def test_400_for_failed_update(self):
+    res = self.client().patch('/plants/2')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 400)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'],'bad request')
+
+  def test_delete_plant(self):
+    res = self.client().delete('/plants/8')
+    data = json.loads(res.data)
+
+    plant = Plant.query.filter(Plant.id==8).one_or_none()
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['id'])
+    self.assertEqual(plant, None)
+
+  def test_422_if_plant_does_not_exists(self):
+    res = self.client().delete('/plants/100')
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 422)
+    self.assertEqual(data['success'],False)
+    self.assertEqual(data['message'], 'unprocessable')
+
+  def test_create_plant(self):
+    res = self.client().post('/plants', json =self.new_plant)
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['created'])
+
+  def test_405_if_plant_creation_not_allowed(self):
+    res = self.client().post('/plants/45', json = self.new_plant)
+    data = json.loads(res.data)
+
+    self.assertEqual(res.status_code, 405)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'method not allowed')
+
+
+
+
+
+if __name__=="__main__":
+  unittest.main()
